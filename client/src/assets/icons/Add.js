e5661f735b3e8562b490fa442b24a96b30d16214
Bukky Oluseye <bukky.oluseye04@gmail.com>
2022-07-21 23:33:37 +0100

    Add icons and convert to js with SVGR

---
+++ client/src/assets/icons/Add.js
@@ -0,0 +1,15 @@
+import * as React from "react";
+
+const Add = (props) => (
+  <svg
+    x={0}
+    y={0}
+    viewBox="0 0 24 24"
+    xmlns="http://www.w3.org/2000/svg"
+    {...props}
+  >
+    <path d="M19 13H5v-2h14v2Z" fill="currentColor" />
+  </svg>
+);
+
+export default Add;
