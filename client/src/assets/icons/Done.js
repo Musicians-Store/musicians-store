ad6bd70e5040e781594c5dfaadfdc7d9a7ff6a0b
Bukky Oluseye <bukky.oluseye04@gmail.com>
2022-07-21 23:33:37 +0100

    Add icons and convert to js with SVGR

---
+++ client/src/assets/icons/Done.js
@@ -0,0 +1,18 @@
+import * as React from "react";
+
+const Done = (props) => (
+  <svg
+    x={0}
+    y={0}
+    viewBox="0 0 24 24"
+    xmlns="http://www.w3.org/2000/svg"
+    {...props}
+  >
+    <path
+      d="m8.6 15.6-4.2-4.2L3 12.8l5.6 5.6 12-12L19.2 5 8.6 15.6Z"
+      fill="currentColor"
+    />
+  </svg>
+);
+
+export default Done;
